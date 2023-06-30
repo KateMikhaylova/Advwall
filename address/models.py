1eacbd382a47231c8386cebbf21a119d6cd47e7b
Kate Mikhaylova <kate.mikhaylova@mail.ru>
2023-06-30 15:04:45 +0300

    User model and blank Country, City, Street models added

---
+++ address/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Country(models.Model):
+    pass
+
+
+class City(models.Model):
+    pass
+
+
+class Street(models.Model):
+    pass
