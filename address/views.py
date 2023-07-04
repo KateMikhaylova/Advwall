c1973df4945cb0626f66787c21726e655fbaff7e
Renat Kalimulin <pianoparrot93@gmail.com>
2023-07-05 00:21:12 +0300

    ModelViewSet was changed to ReadOnlyModelViewSet

--- address/views.py
+++ address/views.py
@@ -1,35 +1,23 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from .models import City, Country, Street
 from .serializers import CitySerializer, CountrySerializer, StreetSerializer
 
 
-class CountryViewSet(ModelViewSet):
+class CountryViewSet(ReadOnlyModelViewSet):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
-    
-    def get_permissions(self):
-        if self.action not in ['list', 'retrieve']:
-            return [IsAdminUser()]
-        return [IsAuthenticated()]
+    permission_classes = [IsAuthenticated, ]
 
 
-class CityViewSet(ModelViewSet):
+class CityViewSet(ReadOnlyModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
-    
-    def get_permissions(self):
-        if self.action not in ['list', 'retrieve']:
-            return [IsAdminUser()]
-        return [IsAuthenticated()]
+    permission_classes = [IsAuthenticated, ]
 
 
-class StreetViewSet(ModelViewSet):
+class StreetViewSet(ReadOnlyModelViewSet):
     queryset = Street.objects.all()
     serializer_class = StreetSerializer
-    
-    def get_permissions(self):
-        if self.action not in ['list', 'retrieve']:
-            return [IsAdminUser()]
-        return [IsAuthenticated()]
+    permission_classes = [IsAuthenticated, ]
